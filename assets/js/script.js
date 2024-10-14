c495c33612494600212ec52e034bcaf96d6c2bd7
Cauan <devcauan@gmail.com>
2024-10-14 20:27:13 -0300

    feat: adicionando checkbox nas tarefas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 const formTask = document.querySelector('.form-task');
 const taskList = document.querySelector('.task-list')
 const deletButton = document.querySelector('.delete-button');
-const tasks = [];
+
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+DisplayTask();
+
 formTask.addEventListener('submit', (event) => {
   event.preventDefault()
   const inputValue = event.target[0].value.trim();
   if (inputValue) {
     tasks.push(inputValue);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
     event.target[0].value = '';
     DisplayTask();
   }
@@ -20,15 +24,29 @@ function DisplayTask(task) {
     const newTask = document.createElement('li');
     newTask.textContent = `${task}`;
 
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('task-checked');
+    checkbox.style.marginLeft = '10px';
+
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        newTask.classList.add('task-checked');
+      } else {
+        newTask.classList.remove('task-checked');
+      }
+    })
+
     const deletButton = document.createElement('button');
     deletButton.classList.add('task-delete');
-    deletButton.textContent = 'Delete';
+    deletButton.textContent = 'X';
     deletButton.style.marginLeft = '10px';
 
     deletButton.addEventListener('click', () => {
       deleteTask(index);
     });
 
+    newTask.appendChild(checkbox);
     newTask.appendChild(deletButton);
     taskList.appendChild(newTask);
   });
@@ -38,5 +56,6 @@ function DisplayTask(task) {
 
 function deleteTask(index) {
   tasks.splice(index, 1);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
   DisplayTask();
 }
